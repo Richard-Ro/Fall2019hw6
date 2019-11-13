52df06732d69756bfe1654f6ef190908beae79ed
unknown <Rorick64@gmail.com>
2019-11-13 12:32:05 -0800

    a

---
+++ public/script.js
@@ -0,0 +1,48 @@
+const sendButton = document.getElementById('sendButton')
+const input = document.getElementById('message')
+
+const outputList = document.getElementById('output-list')
+
+const db = firebase.firestore()
+
+sendButton.addEventListener('click', addData)
+
+function addData() {
+  db.collection('messages')
+    .add({
+      text: input.value,
+      timestamp: Date.now(),
+    })
+    .then(function(docRef) {
+      console.log('Document written with ID: ', docRef.id)
+    })
+    .catch(function(error) {
+      console.error('Error adding document: ', error)
+    })
+}
+
+db.collection('messages').onSnapshot(docs => {
+  outputList.innerHTML = ''
+  docs.forEach(doc => showData(doc.data()))
+})
+
+function showData(data) {
+  const todoItem = document.createElement('li')
+  const todo = document.createTextNode(data.text)
+  todoItem.appendChild(todo)
+  outputList.appendChild(todoItem)
+}
+
+
+
+
+
+// index.html
+//<!-- The core Firebase JS SDK is always required and must be listed first -->
+//<script src="/__/firebase/7.3.0/firebase-app.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+//<!-- Initialize Firebase -->
+//<script src="/__/firebase/init.js"></script>
